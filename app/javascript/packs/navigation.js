8bcbc70060978c264eee1a0f1d7ce343f3dcaddd
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-09-23 02:22:57 +0700

    vue: navigation

---
+++ app/javascript/packs/navigation.js
@@ -0,0 +1,11 @@
+// app/javascript/packs/navigation.js
+import Vue from 'vue'
+import NavigationApp from '../navigation.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new Vue({
+        render: h => h(NavigationApp)
+    }).$mount()
+
+    document.body.insertBefore(app.$el, document.body.firstChild);
+})
