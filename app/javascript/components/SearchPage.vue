19e10831609a3382b35fe54f402ace322315e3cc
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-09-23 03:04:59 +0700

    vue: search page

---
+++ app/javascript/components/SearchPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="">
+      <input type="text" class="bg-gray-200 px-4 py-3 rounded-bl-lg rounded-tl-lg w-10/12" placeholder="Type anything..."/>
+      <button class="bg-yellow-400 text-white px-4 py-3 rounded-br-lg rounded-tr-lg font-bold w-1/12 hover:bg-yellow-500"><i class="fa fa-search"></i></button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchPage',
+}
+</script>
---
+++ app/javascript/giphy.vue
@@ -0,0 +1,21 @@
+<template>
+    <center class="p-5 ">
+        <div class="w-10/12">
+            <div class="w-full mt-5 mb-5 text-left">
+                <span class="mr-5 text-2xl font-bold mb-5 text-gray-600">Search GIF</span>
+            </div>
+            <SearchPage></SearchPage>
+        </div>
+    </center>
+</template>
+
+<script>
+
+import SearchPage from './components/SearchPage.vue';
+export default {
+  components: {
+    SearchPage
+  }
+}
+
+</script>
