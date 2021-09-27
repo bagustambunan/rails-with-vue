5f70cbf699243a234c030978fa905f09b2e6cc64
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-09-27 23:38:13 +0700

    gif component render

--- app/javascript/components/Gif.vue
+++ app/javascript/components/Gif.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="justify-start w-3/12 p-3">
-        <img src={{data.images.original.url}} title={{data.title}} class="rounded-lg" alt={{data.title}}/>
-        <p class="text-sm mt-2 mb-2">{{data.title}}</p>
+        <img src={{img}} title={{title}} class="rounded-lg" alt={{title}}/>
+        <p class="text-sm mt-2 mb-2">{{title}}</p>
+        Halo there
     </div>
 </template>
+
+<script>
+    export default {
+        props: ['img','title']
+    }
+</script>
